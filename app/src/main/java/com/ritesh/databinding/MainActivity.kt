a8cd296de7cbf40838ba5f29e56cce3d4f24625d
riteshsonar <ritesh.sonar111@gmail.com>
2022-01-12 00:28:35 +0530

    Add ViewModel with mutableLiveData

--- app/src/main/java/com/ritesh/databinding/MainActivity.kt
+++ app/src/main/java/com/ritesh/databinding/MainActivity.kt
@@ -2,10 +2,25 @@ package com.ritesh.databinding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.ritesh.databinding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    lateinit var mainViewModel: MainViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //  setContentView(R.layout.activity_main)
+        binding= DataBindingUtil.setContentView(this,R.layout.activity_main)
+        mainViewModel= ViewModelProvider(this).get(MainActivity::class.java)
+
+        mainViewModel.quoteLiveData.observe(this, Observer {
+            binding.textTv.text= it
+        })
+        binding.buttonUpdate.setOnClickListener{
+            mainViewModel.updateQuote()
+        }
     }
 }
---
+++ app/src/main/java/com/ritesh/databinding/MainViewModel.kt
@@ -0,0 +1,11 @@
+package com.ritesh.databinding
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel :ViewModel() {
+    val quoteLiveData = MutableLiveData("What you give is what you get")
+    fun updateQuote(){
+        quoteLiveData.value ="You'll see it when you believe it."
+    }
+}
