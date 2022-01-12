a0676c30b2b31301a2afe69a43200ff01462738a
riteshsonar <ritesh.sonar111@gmail.com>
2022-01-13 00:48:21 +0530

    changes in live data

--- app/src/main/java/com/ritesh/databinding/MainActivity.kt
+++ app/src/main/java/com/ritesh/databinding/MainActivity.kt
@@ -16,9 +16,9 @@ class MainActivity : AppCompatActivity() {
         binding= DataBindingUtil.setContentView(this,R.layout.activity_main)
         mainViewModel= ViewModelProvider(this).get(MainViewModel::class.java)
 
-        val quoteObj= Quote("Do, or do not. There is no try.","Yoda")
-        binding.quote= quoteObj
-        /*mainViewModel.quoteLiveData.observe(this, Observer {
+        binding.mainViewModel = mainViewModel
+        binding.lifecycleOwner = this
+       /* mainViewModel.quoteLiveData.observe(this, Observer {
             binding.quoteTv.text= it
         })*/
        /* binding.buttonUpdate.setOnClickListener{
