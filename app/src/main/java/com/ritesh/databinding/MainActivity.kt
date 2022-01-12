bc9055ef70a10733eaea2540b725e9438f91efdf
riteshsonar <ritesh.sonar111@gmail.com>
2022-01-12 23:32:50 +0530

    Add dependence on it

--- app/src/main/java/com/ritesh/databinding/MainActivity.kt
+++ app/src/main/java/com/ritesh/databinding/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         //  setContentView(R.layout.activity_main)
         binding= DataBindingUtil.setContentView(this,R.layout.activity_main)
-        mainViewModel= ViewModelProvider(this).get(MainActivity::class.java)
+        mainViewModel= ViewModelProvider(this).get(MainViewModel::class.java)
 
         mainViewModel.quoteLiveData.observe(this, Observer {
             binding.textTv.text= it
